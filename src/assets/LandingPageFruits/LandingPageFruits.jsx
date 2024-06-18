09110f5e8c0bf79ac342522010fd3f806c045475
DeiviHerreraDiaz09 <herreradiazdeivistiven2004@gmail.com>
2024-06-18 12:54:19 -0500

    FEAT: Creation Component LandingPageFruits

---
+++ src/assets/LandingPageFruits/LandingPageFruits.jsx
@@ -0,0 +1,11 @@
+import "./LandingPageFruits.css";
+
+const LandingPageFruits = () => {
+    return (
+        <div className='landingpagefruits'>
+            <h1>Landing page One</h1>
+        </div>
+    )
+}
+
+export default LandingPageFruits
